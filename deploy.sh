9bd0f3a6f52a269a93bb1324a73007201f090d63
Linus Yang <laokongzi@gmail.com>
2017-10-13 14:19:20 +0800

    Update

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,8 @@
 declare -a urls=(
 
 # Rom URLs
-"http://bigota.d.miui.com/7.9.27/miui_MIMIX2_7.9.27_544b5231d0_7.1.zip"
+"http://bigota.d.miui.com/7.10.12/miui_MIMIX2_7.10.12_c1c2bdeca7_7.1.zip"
+
 )
 
 command -v dirname >/dev/null 2>&1 && cd "$(dirname "$0")"
