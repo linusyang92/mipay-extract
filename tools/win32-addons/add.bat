2eaedaddd7d1067f4af66a35528c602d2b551fdc
Linus Yang <laokongzi@gmail.com>
2018-01-19 23:57:36 +0800

    Fix Windows version

--- tools/win32-addons/add.bat
+++ tools/win32-addons/add.bat
@@ -4,5 +4,6 @@ copy net.dll ..\win32\jre\bin\
 copy nio.dll ..\win32\jre\bin\
 copy tzdb.dat ..\win32\jre\lib\
 copy tzmappings ..\win32\jre\lib\
+copy jce.jar ..\win32\jre\lib\
 copy find.exe ..\win32\bin\
 popd
