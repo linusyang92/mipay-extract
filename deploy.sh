6e7deb74a158c8a7144b0c4e240d0648bc8b2c6b
linusyang92 <32575696+linusyang92@users.noreply.github.com>
2018-12-15 11:00:51 +0800

    Update to 8.12.13

--- deploy.sh
+++ deploy.sh
@@ -3,16 +3,16 @@
 declare -a urls=(
 
 # Rom URLs
-'http://bigota.d.miui.com/8.12.6/miui_MIMIX2_8.12.6_d0392b49ba_8.0.zip'
+'http://bigota.d.miui.com/8.12.13/miui_MIMIX2_8.12.13_0e27f149d1_8.0.zip'
 
 )
 
-EU_VER=8.12.6
+EU_VER=8.12.13
 
 declare -a eu_urls=(
 
 # EU Rom URLs
-'https://jaist.dl.sourceforge.net/project/xiaomi-eu-multilang-miui-roms/xiaomi.eu/MIUI-WEEKLY-RELEASES/8.12.6/xiaomi.eu_multi_MIMix2_8.12.6_v10-8.0.zip'
+'https://jaist.dl.sourceforge.net/project/xiaomi-eu-multilang-miui-roms/xiaomi.eu/MIUI-WEEKLY-RELEASES/8.12.13/xiaomi.eu_multi_MIMix2_8.12.13_v10-8.0.zip'
 
 )
 
